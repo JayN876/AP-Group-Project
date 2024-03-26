0c3ed4c718578f1eacbfb5f1339e218a279cf5de
Ricardo Ellis <ricardo.ellis12@gmail.com>
2024-03-25 22:17:34 -0500

    Create DateLabelFormatter.java for date picker formatting

---
+++ src/helpers/DateLabelFormatter.java
@@ -0,0 +1,33 @@
+package helpers;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.swing.JFormattedTextField.AbstractFormatter;
+
+public class DateLabelFormatter extends AbstractFormatter {
+
+    /**
+	 * Default serial for JDatePicker https://stackoverflow.com/questions/26794698/how-do-i-implement-jdatepicker
+	 */
+	private static final long serialVersionUID = -8608166058050457356L;
+	private String datePattern = "yyyy-MM-dd";
+    private SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
+
+    @Override
+    public Object stringToValue(String text) throws ParseException {
+        return dateFormatter.parseObject(text);
+    }
+
+    @Override
+    public String valueToString(Object value) throws ParseException {
+        if (value != null) {
+            Calendar cal = (Calendar) value;
+            return dateFormatter.format(cal.getTime());
+        }
+
+        return "";
+    }
+
+}
