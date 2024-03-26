796abdcb8a59c68f9e3c8a626472f47de229ff7d
Ricardo Ellis <ricardo.ellis12@gmail.com>
2024-03-26 00:03:38 -0500

    Create CompanyRepresentative.java for capturing database results

---
+++ src/beans/CompanyRepresentative.java
@@ -0,0 +1,32 @@
+package beans;
+
+public class CompanyRepresentative {
+	private String customerId;
+	
+	public CompanyRepresentative(String customerId, String customer) {
+		super();
+		this.customerId = customerId;
+		this.customer = customer;
+	}
+	
+	private String customer;
+	
+	
+	
+	public String getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
+	
+	public String getCustomer() {
+		return customer;
+	}
+	public void setCustomer(String customer) {
+		this.customer = customer;
+	}
+	
+	
+
+}
