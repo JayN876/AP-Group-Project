19b5bcc6f5218a42634d09e0290a95cc50e4710c
Ricardo Ellis <ricardo.ellis12@gmail.com>
2024-03-25 20:12:30 -0500

    added property loader to load properties dynamically

---
+++ src/helpers/PropertyLoader.java
@@ -0,0 +1,55 @@
+package helpers;
+
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+import driver.Main;
+
+public class PropertyLoader {
+	
+	private final Properties _propertProperties;
+	private final String _resourcePropertyName;
+	private Boolean _isLoaded;
+	public PropertyLoader(String resourcePropertyName) {
+		this._propertProperties = new Properties();
+		this._resourcePropertyName = resourcePropertyName;
+		this._isLoaded = false;
+	}
+
+	public void loadPropertyResource() {
+		this._isLoaded = false;
+		try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(_resourcePropertyName)){
+			
+			this._propertProperties.load(inputStream);
+			this._isLoaded = true;
+		} catch (Exception e) {
+			// TODO: handle exception
+			// TODO: add logger here to show
+			this._isLoaded = false;
+		}
+	}
+	
+	public void addKeyValue(String key, String value) {
+		if(this._isLoaded != false) {
+			this._propertProperties.setProperty(key, value);
+		}
+		else {
+			
+		}
+	}
+	
+	
+	public void save() {
+		try {
+			this._propertProperties.store(new FileOutputStream(_resourcePropertyName), null);
+		} catch (Exception e) {
+			// TODO: handle exception
+			// TODO: add logger here to show 
+		}
+	}
+	
+	public Properties getProperties() {
+		return this._propertProperties;
+	}
+}
