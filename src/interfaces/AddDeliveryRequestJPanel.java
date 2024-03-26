621d04ac96d6479c9b026b2e37baa65f519c870e
Ricardo Ellis <ricardo.ellis12@gmail.com>
2024-03-25 20:20:15 -0500

    started on the add delivery screen

---
+++ src/interfaces/AddDeliveryRequestJPanel.java
@@ -0,0 +1,17 @@
+package interfaces;
+
+import javax.swing.JPanel;
+
+public class AddDeliveryRequestJPanel extends JPanel {
+
+	/**
+	 * Default serial for class
+	 */
+	private static final long serialVersionUID = -748180474167432423L;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
