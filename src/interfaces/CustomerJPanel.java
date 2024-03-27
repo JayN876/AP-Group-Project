89ff9713c2e3e36d8c95055525a0da7ae6526db4
Roberto James <roberto.junior.james13@gmail.com>
2024-03-26 19:16:23 -0500

    Updated form fields on Customer JPanel

--- src/interfaces/CustomerJPanel.java
+++ src/interfaces/CustomerJPanel.java
@@ -10,6 +10,7 @@ import java.awt.Insets;
 import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.security.KeyStore.PrivateKeyEntry;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -41,16 +42,16 @@ public class CustomerJPanel {
 	private JTextField cusIDField;
 	private JLabel company;
 	private JTextField companyField;
-	private JLabel contactPerson;
-	private JTextField contactField;
-	private JLabel address1Label;
-	private JTextField streetNameField1;
-	private JTextField communityField1;
-	private JTextField parishField1;
-	private JLabel address2Label;
-	private JTextField streetNameField2;
-	private JTextField communityField2;
-	private JTextField parishField2;
+	private JLabel emergencyNoLabel;
+	private JTextField emergencyField;
+	private JLabel addressLabel;
+	private JLabel addressLine1Label;
+	private JLabel addressLine2Label;
+	private JTextField addressLine1Field;
+	private JTextField addressLine2Field;
+	private JTextField communityField;
+	private JTextField parishORStateField;
+	private JLabel parishLabel;
 	private JLabel telephoneLabel;
 	private JTextField telephoneField;
 	private JLabel emailLabel;
@@ -61,7 +62,9 @@ public class CustomerJPanel {
 	private JButton clearButton;
 	private GridBagConstraints gbc;
 	private JPanel panel;
-	
+	private JLabel countryLabel;
+	private JLabel communityLabel;
+	private JTextField countryField;
 	
 	public CustomerJPanel() {
 		logger.info("Customer Panel Launched");
@@ -79,180 +82,163 @@ public class CustomerJPanel {
         gbc.gridwidth=2;
         panel.add(headingJLabel, gbc);
         
+        company = new JLabel("Company : ");
+        gbc.gridy=1;
+        panel.add(company,gbc);
         
         fnameLabel = new JLabel("First Name: ");
-        gbc.gridy=1;
+        gbc.gridy=2;
         panel.add(fnameLabel,gbc);
         
         lnameLabel = new JLabel("Last Name: ");
-        gbc.gridy=2;
+        gbc.gridy=3;
         panel.add(lnameLabel,gbc);
         
         gbc = new GridBagConstraints();
         gbc.anchor = GridBagConstraints.NORTHWEST;
         customerID = new JLabel("Customer ID : ");
-        gbc.gridy = 3;
+        gbc.gridy = 4;
         panel.add(customerID,gbc);
-        
-        company = new JLabel("Company : ");
-        gbc.gridy=4;
-        panel.add(company,gbc);
-		
-        contactPerson = new JLabel("Contact Person : ");
+        	
+        emergencyNoLabel = new JLabel("Emergency No : ");
         gbc.gridy=5;
-        panel.add(contactPerson,gbc);
+        panel.add(emergencyNoLabel,gbc);
         
         gbc.gridy=6;
         gbc.gridwidth =2;
         panel.add(new JLabel("________________________________________"),gbc);
         
-        address1Label = new JLabel("Address Line 1 ");
+        addressLabel = new JLabel("Address ");
         gbc.gridy=7;
-        panel.add(address1Label,gbc);
+        panel.add(addressLabel,gbc);
 		
-        address1Label = new JLabel("Street Name : ");
+        addressLine1Label = new JLabel("Address Line 1 : ");
         gbc.gridy=8;
-        panel.add(address1Label,gbc);
+        panel.add(addressLine1Label,gbc);
         
-        address1Label = new JLabel("Community : ");
+        addressLine2Label = new JLabel("<html>Address Line 2 : <br>(Optional)</html>");
         gbc.gridy=9;
-        panel.add(address1Label,gbc);
+        panel.add(addressLine2Label,gbc);
         
-        address1Label = new JLabel("Parish : ");
+        communityLabel = new JLabel("Community : ");
         gbc.gridy=10;
-        panel.add(address1Label,gbc);
+        panel.add(communityLabel,gbc);
         
+        parishLabel = new JLabel("<html>Parish or<br>State : </html> ");
         gbc.gridy=11;
-        gbc.gridwidth =2;
-        panel.add(new JLabel("________________________________________"),gbc);
+        panel.add(parishLabel,gbc);
         
-        address2Label = new JLabel("Address Line 2   (OPTIONAL)");
-        gbc.gridy=12;
-        gbc.gridwidth=2;
-        panel.add(address2Label,gbc);
         
-        address2Label = new JLabel("Street Name : ");
+        countryLabel = new JLabel("Country : ");
+        gbc.gridy=12;       
+        panel.add(countryLabel,gbc);
+        
+        
         gbc.gridy=13;
-        panel.add(address2Label,gbc);
-        
-        address2Label = new JLabel("Community : ");
-        gbc.gridy=14;
-        panel.add(address2Label,gbc);
-        
-        address2Label = new JLabel("Parish : ");
-        gbc.gridy=15;
-        panel.add(address2Label,gbc);
-        
-        gbc.gridy=16;
         gbc.gridwidth =2;
         panel.add(new JLabel("________________________________________"),gbc);
         
         telephoneLabel = new JLabel("Telephone : ");
-        gbc.gridy=17;
+        gbc.gridy=14;
         panel.add(telephoneLabel,gbc);
 
         emailLabel = new JLabel("Email : ");
-        gbc.gridy=18;
+        gbc.gridy=15;
         panel.add(emailLabel,gbc);
         
         statusLabel = new JLabel("Status : ");
-        gbc.gridy=19;
+        gbc.gridy=16;
         panel.add(statusLabel,gbc);
         
         //column 2 Fields
         gbc = new GridBagConstraints();
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        companyField = new JTextField();
+        companyField.setColumns(10);
+        gbc.gridx=1;
+        gbc.gridy=1;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        panel.add(companyField,gbc);
         
         fnameField = new JTextField();
         gbc.gridx =1;
-        gbc.gridy=1;
+        gbc.gridy=2;
         gbc.anchor = GridBagConstraints.NORTHWEST;
         fnameField.setColumns(10);
         panel.add(fnameField,gbc); 
         
         lnameField = new JTextField();
         gbc.gridx =1;
-        gbc.gridy=2;
+        gbc.gridy=3;
         gbc.anchor = GridBagConstraints.NORTHWEST;
         lnameField.setColumns(10);
         panel.add(lnameField,gbc);
         
         cusIDField = new JTextField();
         gbc.gridx =1;
-        gbc.gridy=3;
+        gbc.gridy=4;
         gbc.anchor = GridBagConstraints.NORTHWEST;
         cusIDField.setColumns(10);
         panel.add(cusIDField,gbc);
         
-        companyField = new JTextField();
-        companyField.setColumns(10);
-        gbc.gridx=1;
-        gbc.gridy=4;
-        panel.add(companyField,gbc);
         
-        contactField = new JTextField();
-        contactField.setColumns(10);
+        emergencyField = new JTextField();
+        emergencyField.setColumns(10);
         gbc.gridx=1;
         gbc.gridy=5;
-        panel.add(contactField,gbc);
+        panel.add(emergencyField,gbc);
         
-        streetNameField1 = new JTextField();
-        streetNameField1.setColumns(10);
+        addressLine1Field = new JTextField();
+        addressLine1Field.setColumns(10);
         gbc.gridx=1;
         gbc.gridy=8;
-        panel.add(streetNameField1,gbc);
+        panel.add(addressLine1Field,gbc);
         
-        communityField1 = new JTextField();
-        communityField1.setColumns(10);
+        addressLine2Field = new JTextField();
+        addressLine2Field.setColumns(10);
         gbc.gridx=1;
         gbc.gridy=9;
-        panel.add(communityField1,gbc);
+        panel.add(addressLine2Field,gbc);
         
-        parishField1 = new JTextField();
-        parishField1.setColumns(10);
+        communityField = new JTextField();
+        communityField.setColumns(10);
         gbc.gridx=1;
         gbc.gridy=10;
-        panel.add(parishField1,gbc);
+        panel.add(communityField,gbc);
         
-        streetNameField2 = new JTextField();
-        streetNameField2.setColumns(10);
+        parishORStateField = new JTextField();
+        parishORStateField.setColumns(10);
         gbc.gridx=1;
-        gbc.gridy=13;
-        panel.add(streetNameField2,gbc);
+        gbc.gridy=11;
+        panel.add(parishORStateField,gbc);
         
-        communityField2 = new JTextField();
-        communityField2.setColumns(10);
+        countryField = new JTextField();
+        countryField.setColumns(10);
         gbc.gridx=1;
-        gbc.gridy=14;
-        panel.add(communityField2,gbc);
-        
-        parishField2 = new JTextField();
-        parishField2.setColumns(10);
-        gbc.gridx=1;
-        gbc.gridy=15;
-        panel.add(parishField2,gbc);
-        
+        gbc.gridy=12;
+        panel.add(countryField,gbc);
         
         telephoneField = new JTextField();
         telephoneField.setColumns(10);
         gbc.gridx=1;
-        gbc.gridy=17;
+        gbc.gridy=14;
         panel.add(telephoneField,gbc);
         
         emailField = new JTextField();
         emailField.setColumns(10);
         gbc.gridx=1;
-        gbc.gridy=18;
+        gbc.gridy=15;
         panel.add(emailField,gbc);
         
         String status[]= {"active", "inactive"};
         statusComboBox = new JComboBox<String>(status);
         gbc.gridx=1;
-        gbc.gridy=19;
+        gbc.gridy=16;
         panel.add(statusComboBox,gbc);
         
         confirmButton = new JButton("Confirm");
         gbc.gridx=0;
-        gbc.gridy=20;
+        gbc.gridy=17;
         int topPadding=20;
         gbc.insets = new Insets(topPadding, 0, 0, 0);
         
@@ -262,9 +248,9 @@ public class CustomerJPanel {
                 // Check if all required fields are filled
                 if (fnameField.getText().isEmpty() || lnameField.getText().isEmpty() ||
                         cusIDField.getText().isEmpty() || companyField.getText().isEmpty() ||
-                        contactField.getText().isEmpty() || 
-                        (streetNameField1.getText().isEmpty() && communityField1.getText().isEmpty()) || 
-                        parishField1.getText().isEmpty() ||
+                        emergencyField.getText().isEmpty() || 
+                        (addressLine1Field.getText().isEmpty()&&addressLine2Field.getText().isEmpty()) || 
+                        parishORStateField.getText().isEmpty() || countryField.getText().isEmpty()||
                         telephoneField.getText().isEmpty() || emailField.getText().isEmpty()) {
                 	logger.info("Customer Form was Incomplete");
                     // Display a message indicating that all fields are required
@@ -273,9 +259,8 @@ public class CustomerJPanel {
                 else {//if all mandatory fields are entered
                 	logger.info("All Mandatory fields were entered");
 	                databaseAction.CustomerDatabaseAction cusAction = new CustomerDatabaseAction();
-	                Address address1 = new Address(cusIDField.getText(),streetNameField1.getText(),communityField1.getText() ,parishField1.getText());
-	                Address address2 = new Address(cusIDField.getText(),streetNameField2.getText(),communityField2.getText() ,parishField2.getText());
-	                Customer customer = new Customer(cusIDField.getText(), fnameField.getText(),lnameField.getText(),contactField.getText(),address1 , address2, telephoneField.getText(), emailField.getText(), companyField.getText(),(String)statusComboBox.getSelectedItem() );
+	                Address address = new Address(cusIDField.getText(),addressLine1Field.getText(),addressLine2Field.getText(),communityField.getText() ,parishORStateField.getText(),countryField.getText());
+	                Customer customer = new Customer(cusIDField.getText(), fnameField.getText(),lnameField.getText(),emergencyField.getText(),address, telephoneField.getText(), emailField.getText(), companyField.getText(),(String)statusComboBox.getSelectedItem() );
 	                cusAction.addCustomer(customer);
 	                
                 }
@@ -289,27 +274,27 @@ public class CustomerJPanel {
         
         clearButton = new JButton("Clear");   
         gbc.gridx=1;
-        gbc.gridy=20;
+        gbc.gridy=17;
         gbc.insets = new Insets(topPadding, 0, 0, 0);
         
         clearButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // Set all fields to empty strings
+            	logger.info("All fields were cleared");
                 fnameField.setText("");
                 lnameField.setText("");
                 cusIDField.setText("");
                 companyField.setText("");
-                contactField.setText("");
-                streetNameField1.setText("");
-                communityField1.setText("");
-                parishField1.setText("");
-                streetNameField2.setText("");
-                communityField2.setText("");
-                parishField2.setText("");
+                emergencyField.setText("");
+                addressLine1Field.setText("");
+                addressLine2Field.setText("");
+                communityField.setText("");
+                parishORStateField.setText("");
                 telephoneField.setText("");
                 emailField.setText("");
-                logger.info("All fields were cleared");
+                countryField.setText("");
+                
             }
         });
 
