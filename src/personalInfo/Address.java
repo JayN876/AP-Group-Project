2b97726b490e5583d28915912a376448a321b744
Roberto James <roberto.junior.james13@gmail.com>
2024-03-26 23:08:30 -0500

    adding new fields to Address

--- src/personalInfo/Address.java
+++ src/personalInfo/Address.java
@@ -3,38 +3,75 @@ package personalInfo;
 public class Address {
 	
 	private String id;
-	private String streetName;
+	private String addressLine1;
+	private String addressLine2;
 	private String community;
 	private String parish;
+	private String country;
 	
 	
 	public Address() {
 		this.id="";
-		this.streetName = "";
+		this.addressLine1 = "";
+		this.addressLine2 = "";
 		this.community = "";
 		this.parish = "";
+		this.country = "";
 	}
 	
-	public Address(String id,String streetName, String community, String parish) {
+	
+
+	public Address(String id, String addressLine1, String addressLine2, String community, String parish, String country) {
 		this.id = id;
-		this.streetName = streetName;
+		this.addressLine1 = addressLine1;
+		this.addressLine2 = addressLine2;
 		this.community = community;
 		this.parish = parish;
+		this.country = country;
 	}
+	
+	public Address(String id, String addressLine1, String community, String parish) {
+		this.id = id;
+		this.addressLine1 = addressLine1;
+		this.addressLine2 = addressLine2;
+		this.community = community;
+		this.parish = parish;
+		this.country = country;
+	}
+	
 	
 	public Address(Address address) {
 		this.id = address.id;
-		this.streetName = address.streetName;
+		this.addressLine1 = address.addressLine1;
+		this.addressLine2 = address.addressLine2;
 		this.community = address.community;
 		this.parish = address.parish;
+		this.country = address.country;
 	}
 
-	public String getStreetName() {
-		return streetName;
+
+	public String getId() {
+		return id;
 	}
 
-	public void setStreetName(String streetName) {
-		this.streetName = streetName;
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+
+	public String getAddressLine2() {
+		return addressLine2;
+	}
+
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
 	}
 
 	public String getCommunity() {
@@ -53,28 +90,20 @@ public class Address {
 		this.parish = parish;
 	}
 
-	public String getId() {
-		return id;
+	public String getCountry() {
+		return country;
 	}
 
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	@Override
-	public String toString() {
-		return "Address [id=" + id + ", streetName=" + streetName + ", community=" + community + ", parish=" + parish
-				+ "]";
+	public void setCountry(String country) {
+		this.country = country;
 	}
 
 	public boolean isEmpty() {
-		int addressStringLength=streetName.length()+ community.length()+parish.length();
+		int addressStringLength=addressLine1.length() +addressLine2.length()+ community.length()+parish.length()+country.length();
 		if (addressStringLength>0) {
 			return false;
 		}
 		return true;
 	}
 
-	
-	
 }
